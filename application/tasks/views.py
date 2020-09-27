7ecf625f5d9fe1410b2262fee388ee2137fd376a
Aare <aare212@hotmail.com>
2020-09-27 17:01:33 +0300

    Adding new tasks

---
+++ application/tasks/views.py
@@ -0,0 +1,20 @@
+from application import app, db
+from flask import render_template, request, redirect, url_for
+from application.tasks.models import Task
+
+@app.route("/tasks/new/")
+def tasks_form():
+    return render_template("tasks/new.html")
+
+@app.route("/tasks", methods=["GET"])
+def tasks_index():
+    return render_template("tasks/list.html", tasks = Task.query.all())
+
+@app.route("/tasks/", methods=["POST"])
+def tasks_create():
+    t = Task(request.form.get("name"))
+
+    db.session().add(t)
+    db.session().commit()
+
+    return redirect(url_for("tasks_index"))

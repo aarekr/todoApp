29a5905d9571b02ead04bf009f635a6b1d3d12ef
Aare <aare212@hotmail.com>
2020-09-29 18:04:44 +0300

    Class TaskForm implemented

--- application/tasks/views.py
+++ application/tasks/views.py
@@ -1,10 +1,11 @@
 from application import app, db
 from flask import render_template, request, redirect, url_for
 from application.tasks.models import Task
+from application.tasks.forms import TaskForm
 
 @app.route("/tasks/new/")
 def tasks_form():
-    return render_template("tasks/new.html")
+    return render_template("tasks/new.html", form = TaskForm())
 
 @app.route("/tasks", methods=["GET"])
 def tasks_index():

9b470b46133c90fa9a99d70d2c876171e49676bb
Aare <aare212@hotmail.com>
2020-10-05 21:56:07 +0300

    Logout

--- application/auth/views.py
+++ application/auth/views.py
@@ -1,5 +1,5 @@
 from flask import render_template, request, redirect, url_for
-from flask_login import login_user
+from flask_login import login_user, logout_user
 
 from application import app
 from application.auth.models import User
@@ -18,6 +18,10 @@ def auth_login():
         return render_template("auth/loginform.html", form = form,
                                error = "No such username or password")
 
-
     login_user(user)
+    return redirect(url_for("index"))
+
+@app.route("/auth/logout")
+def auth_logout():
+    logout_user()
     return redirect(url_for("index"))

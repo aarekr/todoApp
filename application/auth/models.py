434bab2c002ef8f1965cdfbe2f383201151e4a20
Aare <aare212@hotmail.com>
2020-10-09 19:10:29 +0300

    Class Base combines Task and User

--- application/auth/models.py
+++ application/auth/models.py
@@ -1,13 +1,9 @@
 from application import db
+from application.models import Base
 
-class User(db.Model):
+class User(Base):
 
     __tablename__ = "account"
-
-    id = db.Column(db.Integer, primary_key=True)
-    date_created = db.Column(db.DateTime, default=db.func.current_timestamp())
-    date_modified = db.Column(db.DateTime, default=db.func.current_timestamp(),
-                              onupdate=db.func.current_timestamp())
 
     name = db.Column(db.String(144), nullable=False)
     username = db.Column(db.String(144), nullable=False)
@@ -17,8 +13,8 @@ class User(db.Model):
 
     def __init__(self, name, username, password):
         self.name = name
-        self.username = username
-        self.password = password
+        #self.username = username
+        #self.password = password
 
     def get_id(self):
         return self.id

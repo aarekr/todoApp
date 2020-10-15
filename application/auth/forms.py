3957fb5bd49ea3e8d723d54fe9e1859882064f0b
Aare <aare212@hotmail.com>
2020-10-15 21:59:33 +0300

    Username validations

--- application/auth/forms.py
+++ application/auth/forms.py
@@ -1,5 +1,7 @@
 from flask_wtf import FlaskForm
-from wtforms import PasswordField, StringField, SubmitField
+from wtforms import PasswordField, StringField, SubmitField, validators
+from wtforms.validators import ValidationError, DataRequired, EqualTo
+from application.auth.models import User
 
 class LoginForm(FlaskForm):
     username = StringField("Username")
@@ -9,9 +11,14 @@ class LoginForm(FlaskForm):
         csrf = False
 
 class NewUserForm(FlaskForm):
-    username = StringField("Username")
+    username = StringField("Username", [validators.Length(min=3, max=10, message="Username has to contain 3-10 characters")])
     password = PasswordField("Password")
-    submit = SubmitField("Create user account")
+    submit = SubmitField("Create account")
+
+    def validate_username(self, username):
+        k = User.query.filter_by(username=username.data).first()
+        if k is not None: # if user account already created
+            raise ValidationError('Choose another username')
 
     class Meta:
         csrf = False
